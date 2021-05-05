a0b2610172023ab43214ce0c1c30d9e6c552f44a
S4kyt <jerikolo1@gmail.com>
2021-05-05 18:44:01 +0200

    add sides of dice input

--- main.cpp
+++ main.cpp
@@ -2,25 +2,30 @@
 #include <random>
 #include <array>
 
-int randGen()
+int randGen(int diceSideInput)
 {
     std::random_device rd;  //Will be used to obtain a seed for the random number engine
     std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
-    std::uniform_int_distribution<> distrib(1, 9); // Here is where you can change the MIN and MAX values of the random dib.
+    std::uniform_int_distribution<> distrib(1, diceSideInput); // Here is where you can change the MIN and MAX values of the random dib.
 
     int randResult = distrib(gen);
     return randResult;
 }
 
 int main() {
+    // Get sides of dice for rolls
+    int diceSideInput;
+    std::cout << "Enter the type of dice you'd like to roll:" << std::endl;
+    std::cin >> diceSideInput;
+
     const int numOfRolls = 9;
 
-    std::array<int, numOfRolls> diceRolls{0, 0, 0, 0, 0, 0, 0, 0, 0,};
+    std::array<int, numOfRolls> diceRolls{};
 
+    // Loop through dice rolls
     for (auto it = diceRolls.begin(); it != diceRolls.end(); it++) {
-        *it = randGen();
+        *it = randGen(diceSideInput);
         std::cout << " " << *it;
     }
-
     return 0;
 }
