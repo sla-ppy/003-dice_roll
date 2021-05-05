26dfbe8101278f1a6a741706c8694d8a7cf8bc3e
S4kyt <jerikolo1@gmail.com>
2021-05-05 20:32:06 +0200

    add dice roll loop so you can roll more with less effort

--- main.cpp
+++ main.cpp
@@ -13,19 +13,39 @@ int randGen(int diceSideInput)
 }
 
 int main() {
-    // Get sides of dice for rolls
-    int diceSideInput;
-    std::cout << "Enter the type of dice you'd like to roll:" << std::endl;
-    std::cin >> diceSideInput;
+    bool rollingDice = true;
 
-    const int numOfRolls = 9;
+    while (rollingDice) {
+        system("cls");
+        // Get sides of dice for rolls
+        int diceSideInput;
+        std::cout << "Enter the type of dice you'd like to roll:" << std::endl;
+        std::cin >> diceSideInput;
 
-    std::array<int, numOfRolls> diceRolls{};
+        const int numOfRolls = 1;
 
-    // Loop through dice rolls
-    for (auto it = diceRolls.begin(); it != diceRolls.end(); it++) {
-        *it = randGen(diceSideInput);
-        std::cout << " " << *it;
+        std::array<int, numOfRolls> diceRolls{};
+
+        // Loop through dice rolls
+        for (auto it = diceRolls.begin(); it != diceRolls.end(); it++) {
+            *it = randGen(diceSideInput);
+            std::cout << "Result of the roll: " << *it << std::endl;
+        }
+
+        std::string rollAgainInput = "";
+        std::cout << "Roll again? Y/N" << std::endl;
+        std::cin >> rollAgainInput;
+
+        if (rollAgainInput == "Y" || rollAgainInput == "y") {
+            rollingDice = true;
+        }
+        else if (rollAgainInput == "N" || rollAgainInput == "n") {
+            rollingDice = false;
+        }
+        else {
+            std::cout << "Enter a valid option. Y/N" << std::endl;
+        }
+
     }
     return 0;
 }
